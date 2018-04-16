98f87658fcf9fae934dfdcc7e38762ce21f25c25
Fatih DURMUŞ <durmus.fatih@yoncabt.com.tr>
2018-04-16 17:39:42 +0300

    ilk commit

---
+++ app/src/main/java/com/modapinhan/fdurmus/modapinhan/MainActivity.java
@@ -0,0 +1,48 @@
+package com.modapinhan.fdurmus.modapinhan;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webview);
+
+        initWebView();
+        myWebView.loadUrl("http://modapinhan.1ticaret.com/");
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+
+
+    }
+
+    private void initWebView() {
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient());
+        WebSettings settings = myWebView.getSettings();
+        settings.setSavePassword(true);
+        settings.setSaveFormData(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setSupportZoom(false);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setDomStorageEnabled(true);
+        settings.setSupportMultipleWindows(false);
+
+    }
+
+
+}
