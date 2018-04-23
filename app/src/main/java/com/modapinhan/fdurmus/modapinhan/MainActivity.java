885e7b364f079867e6558f61db525b8efa10453c
Fatih DURMUŞ <durmus.fatih@yoncabt.com.tr>
2018-04-23 14:12:34 +0300

    Üst Action bara logo kondu

--- app/src/main/java/com/modapinhan/fdurmus/modapinhan/MainActivity.java
+++ app/src/main/java/com/modapinhan/fdurmus/modapinhan/MainActivity.java
@@ -1,18 +1,12 @@
 package com.modapinhan.fdurmus.modapinhan;
 
-import android.content.Intent;
-import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
-import android.graphics.drawable.Icon;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
         myWebView = (WebView) findViewById(R.id.webview);
 
         initWebView();
